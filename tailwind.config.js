97a2949b70474b3a7ab2d10e3fb35171ba22fa25
Andrew <andrewrgpires@gmail.com>
2024-09-05 12:25:38 -0300

    config variables colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     fontFamily: {
       'hanken':['Hanken Grotesk', 'sans-serif']
     },
-    extend: {},
+    extend: {
+      colors: {
+        'lightRed':'#FF5757',
+        'orangeyYellow':'#FFB01F',
+        'greenTeal':'#00BD91',
+        'cobaltBlue':'#1125D4',
+
+        'lightSlateBlue':'#7857FF',
+        'lightRoyalBlue':'#2E2BE9',
+        'violetBlue':'#4E21CA',
+        'persianBlue':'#2421CA',
+
+        'paleBlue':'#EBF1FF',
+        'lightLavender':'#C8C7FF',
+        'darkGrayBlue':'#303B5A',
+      }
+    },
   },
   plugins: [],
 }
