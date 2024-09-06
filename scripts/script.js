1943573c6307bfca858e9d1490212f2b0b377df8
Andrew <andrewrgpires@gmail.com>
2024-09-06 11:08:58 -0300

    consuming json through javascript

--- scripts/script.js
+++ scripts/script.js
@@ -4,26 +4,49 @@ function show() {
         .then(datas => {
             const cont_ = document.querySelector("#results");
 
-            datas.forEach(data => {
+            cont_.innerHTML = '';
+
+            const backgroundColors = [
+                "bg-lightRed",  
+                "bg-orangeyYellow", 
+                "bg-greenTeal",
+                "bg-cobaltBlue"    
+            ];
+
+            const textColors = [
+                "text-lightRed",  
+                "text-orangeyYellow", 
+                "text-greenTeal",
+                "text-cobaltBlue"   
+            ];
+
+            datas.forEach((data, index) => {
                 const card = document.createElement("div");
-                card.classList.add("bg-white", "p-4", "rounded-lg", "shadow-lg", "m-4", "flex", "items-center", "space-y-2");
-
-                const category = document.createElement("p");
-                category.textContent = data.category; 
-                category.classList.add("text-lg", "font-semibold", "text-gray-800");
-
-                const score = document.createElement("p");
-                score.textContent = data.score; 
-                score.classList.add("text-sm", "text-gray-600");
+                card.classList.add("flex", "items-center", "justify-around", "w-80", "h-14", "rounded-xl", "bg-opacity-10");
+                card.classList.add(backgroundColors[index % backgroundColors.length]);
 
                 const icon = document.createElement("img");
                 icon.src = data.icon;
                 icon.alt = "Icon";
-                icon.classList.add("w-12", "h-12", "object-cover");
+                icon.classList.add("h-5", "w-5", "mx-4"); 
 
+                const category = document.createElement("p");
+                category.textContent = data.category + ((index === 2 || index === 3) ? '\u00A0\u00A0\u00A0' : '');
+                category.classList.add("font-bold", "text-lg", "text-cobaltBlue", "mr-28",);
+                category.classList.add(textColors[index % textColors.length]); 
+
+                const score = document.createElement("p");
+                score.textContent = data.score;
+                score.classList.add("font-bold",); 
+
+                const total = document.createElement("p");
+                total.textContent = '/ 100';
+                total.classList.add("text-gray-400", "font-semibold", "mr-4", "ml-2");
+
+                card.appendChild(icon);
                 card.appendChild(category);
                 card.appendChild(score);
-                card.appendChild(icon);
+                card.appendChild(total);
 
                 cont_.appendChild(card);
             });
